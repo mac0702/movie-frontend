b62e1d7f9049ffba96727cec87dbe2357a91fcec
mac0702 <mahekgupta0702@gmail.com>
2025-04-22 14:53:29 +0530

    link updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-    import.meta.env.VITE_BACKEND_BASE_URL || "https://movie-store-backend.onrender.com";
+    import.meta.env.VITE_BACKEND_BASE_URL || "https://movie-store-backend-5pye.onrender.com";
